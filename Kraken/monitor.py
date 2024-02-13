7cd894ee00c0a1b72f8c13495b32bf3ccb092321
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-02-13 10:12:38 +0000

    Update monitor.py

--- Kraken/monitor.py
+++ Kraken/monitor.py
@@ -4,6 +4,8 @@ import pandas as pd
 sys.path.append('config/')
 import config
 
+startTime = time.time()
+
 def get_kraken_signature(urlpath, data, secret):
     postdata = urllib.parse.urlencode(data)
     encoded = (str(data['nonce']) + postdata).encode()
@@ -22,8 +24,10 @@ def kraken_request(uri_path, data):
 dfOriginal = ''
 counter = 0
 
+print(f"Initial start time: {datetime.datetime.fromtimestamp(startTime).strftime('%H:%M:%S')}")
 while True:
-    print(f"Starting loop...\nLoop has run {counter} times.\nSending Request.")
+    startLoop = time.time()
+    print(f"Starting loop...\nLoop has run {counter} times.\nStarting at: {datetime.datetime.fromtimestamp(startLoop).strftime('%H:%M:%S')}\nTotal run time: {datetime.datetime.fromtimestamp(startLoop-startTime).strftime('%H:%M:%S')}\nSending Request.")
     try:
         resp = kraken_request(config.endpoint, {
             "nonce": str(int(1000 * time.time())),
@@ -33,16 +37,18 @@ while True:
         dfOriginal = pd.DataFrame(resp['result'])
         print('Dataframes setup correctly')
         try:
-            #print(pd.concat([dfOriginal,dfNew]).drop_duplicates(keep=False), 'Sleeping for {config.sleepTime} minutes...\n###################################')
+            #print(pd.concat([dfOriginal,dfNew]).drop_duplicates(keep=False), 'Sleeping for {config.sleepTime} minutes\nSleep starting at: {datetime.datetime.fromtimestamp(startSleep).strftime('%H:%M:%S')}\n###################################')
             df = pd.concat([dfNew,dfOriginal]).drop_duplicates(keep=False)
             df = df.to_dict(orient='records')
             try:
-                requests.get(f"https://api.telegram.org/bot{config.telegram_token}/sendMessage?chat_id={config.telegram_id}&text=New Deposit{df[0]}")
+                requests.get(f"https://api.telegram.org/bot{config.telegram_token}/sendMessage?chat_id={config.telegram_id}&text=New Deposit{df}")
             except:
-                requests.get(f"https://api.telegram.org/bot{config.telegram_token}/sendMessage?chat_id={config.telegram_id}&text=New Deposit{df[0]}")
-            print('Sleeping for {config.sleepTime} minutes...\n###################################')
+                requests.get(f"https://api.telegram.org/bot{config.telegram_token}/sendMessage?chat_id={config.telegram_id}&text=New Deposit{df}")
+            startSleep = time.time()
+            print(f"Sleeping for {config.sleepTime} minutes\nSleep starting at: {datetime.datetime.fromtimestamp(startSleep).strftime('%H:%M:%S')}\nSleep finishing at: {datetime.datetime.fromtimestamp(startSleep+300).strftime('%H:%M:%S')}\n###################################")
         except Exception as e:
-            print(f"There was an error concatinating and removing duplicates:\n{e}\nSleeping for {config.sleepTime} minutes...\n###################################")
+            startSleep = time.time()
+            print(f"There was an error concatinating and removing duplicates:\n{e}\nSleeping for {config.sleepTime} minutes\nSleep starting at: {datetime.datetime.fromtimestamp(startSleep).strftime('%H:%M:%S')}\nSleep finishing at: {datetime.datetime.fromtimestamp(startSleep+300).strftime('%H:%M:%S')}\n###################################")
             #requests.get(f"https://api.telegram.org/bot{config.telegram_token}/sendMessage?chat_id={config.telegram_id}&text=Error: {e}")
         counter += 1
         time.sleep(60*config.sleepTime)
